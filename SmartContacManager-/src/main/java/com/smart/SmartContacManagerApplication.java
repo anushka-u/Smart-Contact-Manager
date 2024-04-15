090e59edb7b17d5b80a1170f6c200bd856295acf
anushka-u <anushkau032@gmail.com>
2024-04-15 12:29:46 +0530

    Project SetUp

---
+++ SmartContacManager-/src/main/java/com/smart/SmartContacManagerApplication.java
@@ -0,0 +1,13 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContacManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContacManagerApplication.class, args);
+	}
+
+}
