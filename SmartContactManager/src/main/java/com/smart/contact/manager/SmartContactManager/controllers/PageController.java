1bca0a0c073a81b7c5d5e87ffb4004e1c5afb285
anushka-u <anushkau032@gmail.com>
2024-11-27 07:44:51 +0530

    Smart Contact Manager: updating page controller

--- SmartContactManager/src/main/java/com/smart/contact/manager/SmartContactManager/controllers/PageController.java
+++ SmartContactManager/src/main/java/com/smart/contact/manager/SmartContactManager/controllers/PageController.java
@@ -3,6 +3,7 @@ package com.smart.contact.manager.SmartContactManager.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,8 +11,10 @@ public class PageController {
     private static final Logger logger = LoggerFactory.getLogger(PageController.class);
 
     @GetMapping("/home")
-    public String home() {
-        logger.info("Home page was accessed");  // Logs when the method is hit
+    public String home(Model model) {
+        logger.info("Home page was accessed");// Logs when the method is hit
+        model.addAttribute("name","Substring Technologies");
+        model.addAttribute("githubRepo","https://github.com/anushka-u");
         return "home";  // Matches the home.html template
     }
 }
